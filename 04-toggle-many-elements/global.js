8357d1bc7948eed8f3ee9d7ebea404339e93311c
Ivans Aleskevics <vanjaaleskevic@mail.ru>
2021-03-22 23:00:24 +0200

    4uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    var buttonClick = document.getElementById('button');
+    buttonClick.addEventListener('click', function(){
+        var toggleMe = document.getElementsByClassName('toggle_me');
+        for (var i = 0; i < toggleMe.length; i++) {
+            if (toggleMe[i].style.display === "none") {
+                toggleMe[i].style.display = "block";
+            }
+            else {
+                toggleMe[i].style.display = "none"
+            }
+        }
+    });
+})
