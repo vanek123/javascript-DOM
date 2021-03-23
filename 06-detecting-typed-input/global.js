000a4d070f2133db235e1f09b9f1f39792df8af6
Ivans Aleskevics <vanjaaleskevic@mail.ru>
2021-03-23 14:04:01 +0200

    6uzdevums

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,8 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    var fullName = document.getElementById('full_name');
+    fullName.addEventListener("keyup", function(){
+        var greeting = document.getElementById('greeting');
+        greeting.innerHTML = ("Hello there, " + fullName.value + "!");
+    });
+});
