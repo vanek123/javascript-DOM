e8d3b6e28302e4d9664ec5e12f9b7869fffa5621
Ivans Aleskevics <vanjaaleskevic@mail.ru>
2021-03-23 15:29:23 +0200

    7uzdevums

--- 07-automatic-tabbing-fields/QUESTIONS.md
+++ 07-automatic-tabbing-fields/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > What are some refactoring techniques you might apply to the provided solution?
 
-Your reply here...
+Kad pirmajā logā ir maksimāls ciparu skaits, kursos pāriet uz nākamo logu un kad tajā ir maksimāls ciapru skaits, kursos pāriet uz trešo logu un kad tur ir maksimāls ciparu skaits, kursos nepāriet, viņš apstājas un vairs nevar ievadīt ciparus.
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,25 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    var firstBox = document.getElementById('first');
+    var secondBox = document.getElementById('second');
+    var thirdBox = document.getElementById('third');
+  
+    firstBox.addEventListener("keyup", function() {
+      typedValue = firstBox.value.length;
+      maximum = parseInt(firstBox.getAttribute("maxlength"));
+  
+      if (typedValue === maximum) {
+        secondBox.focus();
+      }
+    });
+  
+    secondBox.addEventListener("keyup", function() {
+      typedValue = secondBox.value.length;
+      maximum = parseInt(secondBox.getAttribute("maxlength"));
+  
+      if (typedValue === maximum) {
+        thirdBox.focus();
+      }
+    });
+  
+  });
